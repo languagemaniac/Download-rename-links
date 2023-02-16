afee0b2a67b847effe70f2b96c32d6e98043ea07
languagemaniac <43100450+languagemaniac@users.noreply.github.com>
2023-02-16 14:22:06 +0100

    Update LinkDownloader.py

--- LinkDownloader.py
+++ LinkDownloader.py
@@ -4,62 +4,62 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 
 # Name of the file where the URLs are
-archivo_urls = "urls.txt"
+url_file = "urls.txt"
 
 # File download folder
-directorio_descarga = "Downloads"
+download_folder = "Downloads"
 
 # Get the number of the last downloaded file
-if not os.path.exists(directorio_descarga):
-    os.makedirs(directorio_descarga)
-files = os.listdir(directorio_descarga)
+if not os.path.exists(download_folder):
+    os.makedirs(download_folder)
+files = os.listdir(download_folder)
 last_file_num = max([int(f.split(".")[0]) for f in files]) if files else 0
 
 # Open the URL list textfile
-with open(archivo_urls, "r", encoding="utf-8") as f:
+with open(url_file, "r", encoding="utf-8") as f:
     urls = f.read().splitlines()
 
 # Ask the user for the number from which to continue downloading
-continuar_desde = input("From what file number do you want to continue downloading? (0 to start from the beginning): ")
+continue_from = input("From what file number do you want to continue downloading? (0 to start from the beginning): ")
 try:
-    continuar_desde = int(continuar_desde)
-    if continuar_desde < 0:
-        continuar_desde = 0
+    continue_from = int(continue_from)
+    if continue_from < 0:
+        continue_from = 0
 except:
-    continuar_desde = 0
+    continue_from = 0
 
 # Download files in parallel
-def descargar_archivo(url, nombre_archivo):
-    ruta_archivo = os.path.join(directorio_descarga, nombre_archivo)
+def download_file(url, file_name):
+    file_path = os.path.join(download_folder, file_name)
 
     # Download files
-    print(f"Downloading {nombre_archivo}...")
+    print(f"Downloading {file_name}...")
     try:
         response = requests.get(url)
-        with open(ruta_archivo, "wb") as f:
+        with open(file_path, "wb") as f:
             f.write(response.content)
-        print(f"{nombre_archivo} Downloaded.")
+        print(f"{file_name} Downloaded.")
     except Exception as e:
-        print(f"Error downloading {nombre_archivo}: {str(e)}")
+        print(f"Error downloading {file_name}: {str(e)}")
         # Wait some time and try again
         time.sleep(60)
         try:
             response = requests.get(url)
-            with open(ruta_archivo, "wb") as f:
+            with open(file_path, "wb") as f:
                 f.write(response.content)
-            print(f"{nombre_archivo} Downloaded.")
+            print(f"{file_name} Downloaded.")
         except Exception as e:
-            print(f"Error when downloading {nombre_archivo}: {str(e)}")
+            print(f"Error when downloading {file_name}: {str(e)}")
             # Wait some more time and try again
             time.sleep(120)
     
 with ThreadPoolExecutor() as executor:
     # Download files in parallel
     futures = []
-    for i, url in enumerate(urls[continuar_desde:]):
-        num_archivo = i + continuar_desde + 1
-        nombre_archivo = f"{num_archivo}.mp3"
-        futures.append(executor.submit(descargar_archivo, url, nombre_archivo))
+    for i, url in enumerate(urls[continue_from:]):
+        file_num = i + continue_from + 1
+        file_name = f"{file_num}.mp3"
+        futures.append(executor.submit(download_file, url, file_name))
         
     # Wait for all the downloads to finish
     for future in futures:
@@ -68,20 +68,20 @@ with ThreadPoolExecutor() as executor:
 print("Download completed")
 
 # Create list with the URLs that couldn't be downloaded and the reason for that
-errores_descarga = []
-for i, url in enumerate(urls[continuar_desde:]):
-    num_archivo = i + continuar_desde + 1
-    nombre_archivo = f"{num_archivo}.mp3"
-    ruta_archivo = os.path.join(directorio_descarga, nombre_archivo)
+download_errors = []
+for i, url in enumerate(urls[continue_from:]):
+    file_num = i + continue_from + 1
+    file_name = f"{file_num}.mp3"
+    file_path = os.path.join(download_folder, file_name)
 
-    if not os.path.exists(ruta_archivo):
-        errores_descarga.append((url, "File not downloaded"))
-    elif os.path.getsize(ruta_archivo) == 0:
-        errores_descarga.append((url, "Empty file"))
+    if not os.path.exists(file_path):
+        download_errors.append((url, "File not downloaded"))
+    elif os.path.getsize(file_path) == 0:
+        download_errors.append((url, "Empty file"))
 
 # Write error list in a text file
-if errores_descarga:
+if download_errors:
     with open("errors.txt", "w", encoding="utf-8") as f:
-        for url, mensaje in errores_descarga:
-            f.write(f"{url}\t{mensaje}\n")
+        for url, message in download_errors:
+            f.write(f"{url}\t{message}\n")
     print("Download errors have been recorded. Check the 'errors.txt' file for more information.")
